16811b7422ebda5e74dcc1bb24e64d257f0ddab9
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-01-03 17:14:15 -0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (99.77%) - LeetSync

--- 1098-largest-unique-number/largest-unique-number.py
+++ 1098-largest-unique-number/largest-unique-number.py
@@ -1,19 +1,14 @@
 from collections import defaultdict
+
 class Solution:
-    def largestUniqueNumber(self, nums: List[int]) -> int:
-    # Create a HashMap with all of the counts of all of the elements
-        # Iterate over nums, and assign key, value pairs to the HashMap
-    # Sort numbers lowest to highest, loop starting from the end of the sorted list
-        # If the value == 1
-            # We can return the key
-        # Return -1
-        hSet = defaultdict(int) # Create the HSet
-        for num in nums: # Loop through the array
-            hSet[num] += 1 # Counting frequency of num
-        for countedElement in sorted(hSet.keys(), reverse = True): # Loop through the counted elements
-            if hSet[countedElement] == 1:
-                return countedElement
-        return -1
-        
-    
-    
+    def largestUniqueNumber(self, nums):
+        hSet = defaultdict(int)
+        for num in nums:
+            hSet[num] += 1
+
+        ans = -1
+        for num, count in hSet.items():
+            if count == 1:
+                ans = max(ans, num)
+
+        return ans
