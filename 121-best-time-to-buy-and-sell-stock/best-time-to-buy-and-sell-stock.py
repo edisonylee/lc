08e71362ec754c947749ec1097563e9ee0b4c994
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-01-04 16:56:11 -0500

    Time: 95 ms (51.35%) | Memory: 26.4 MB (81.04%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Given an array with prices
+        # Return max profit - highest prices in the week - lowest price in the week in order
+        # [ 7, 1, 5, 3, 6, 4]
+        #  I want to find the lowest price of the week, loop until len - 1
+        # I want to find the highest price of the week, loop until len
+        ans = 0
+        left = 0
+        for right in range(1, len(prices)): # looping through to find the buy day
+            if prices[right] < prices[left]: #  comparing the price 
+                if prices[right] != len(prices): # don't consider last element as a buy day
+                    left = right # if right is lower than swap
+            elif prices[right] > prices[left]:
+                ans = max(ans, prices[right] - prices[left])
+        return ans
+                
+
