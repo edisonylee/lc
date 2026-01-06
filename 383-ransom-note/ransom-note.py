1a00caa182f05cfcd1f1596af7a6f1a354811c71
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-01-05 22:07:15 -0500

    Time: 3 ms (98.62%) | Memory: 18 MB (25.17%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,21 +1,6 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        noteSet = {}
-        for char in ransomNote:
-            if char not in noteSet:
-                noteSet[char] = 1
-            else:
-                noteSet[char] += 1
-        magSet = {}
-        for char in magazine:
-            if char in ransomNote:
-                if char not in magSet:
-                    magSet[char] = 1
-                else:
-                    magSet[char] += 1
-        for letter, freq in noteSet.items():
-            if letter not in magSet:
-                return False
-            if freq > magSet[letter]:
+        for char in set(ransomNote):
+            if ransomNote.count(char) > magazine.count(char):
                 return False
         return True
