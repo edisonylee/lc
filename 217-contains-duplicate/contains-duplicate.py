6fbfad91f41bc0011c671dec08252b848316a251
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-01-09 16:34:17 -0500

    Time: 11 ms (72.59%) | Memory: 32.2 MB (22.59%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,10 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = set() # Make a set 
-        count = 0
+        # Given nums, return T if num appears more than once, F is every element distinct
+        # Initial thoughts: I want to loop through nums.
+        # Then, I want to check to see if it's in the set, and if it is not in the set Im going to add it to the set.
+        # Then we can return True if it's in the set, False otherwise.
+        seen = set()
         for num in nums:
             if num in seen:
                 return True
             seen.add(num)
         return False
-        
