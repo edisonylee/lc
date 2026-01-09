22ac2c50b04625fa6132c103be38731352874e6e
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-01-09 17:09:30 -0500

    Time: 15 ms (42.25%) | Memory: 19.5 MB (5.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,20 +1,24 @@
-from collections import defaultdict
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Optimization: If lengths differ, they can't be anagrams
-        if len(s) != len(t):
-            return False
-            
-        dic = defaultdict(int)
-        dicTee = defaultdict(int)
-        
-        # Loop through first string
-        for char in s:
-            dic[char] += 1  # Just increment; defaultdict handles the rest
-            
-        # Loop through second string
-        for char in t:
-            dicTee[char] += 1
-            
-        return dic == dicTee
+        # So, given two strings, s and t, return true if t is an anagram of s,
+        # And false otherwise
+        # So, what is an anagram?
+        # An anagram is any string that contains all the letters, however mixed up, as another string
+        # With this in mind, we can start our implementation.
+        # Initial approach: Create dictionaries for each each string, then we can compare them by using .items
+        # This will allow us to fetch the appropriate key, value pairs. We can either loop through and compare 
+        # with a for loop, or just do the == for the hashMaps. 
+        # Let's try both out.
+        dicS = {}
+        dicT = {}
+        for chr in s:
+            if chr in dicS:
+                dicS[chr] += 1
+            else: 
+                dicS[chr] = 1
+        for chr in t:
+            if chr in dicT:
+                dicT[chr] += 1
+            else: 
+                dicT[chr] = 1
+        return dicS == dicT
