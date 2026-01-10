1ee4383f0c97895123e7f175d80bb298b438324d
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-01-10 16:34:37 -0500

    Time: 7 ms (53.99%) | Memory: 24.9 MB (7.29%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,19 @@
-from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        dic = defaultdict(int) # init hashmap
-        for num in nums: # add numbers to hashmap
-            dic[num] += 1
-        sorted_items = sorted(dic.items(), key=lambda x:x[1], reverse = True)
-        result = []
-        for pair in sorted_items:
-            num = pair[0]
-            result.append(num)
-            if len(result) == k:
-                break
-        return result
-
-        
+        # Given an integer array nums and an integer k, 
+        # Return the k most frequent elements
+        # You may return the answer in any order.
+        count = {} # first we initialize hashmap
+        freq = [] # initiliaze an empty list  = [[], [3],[2] ,[1] ,[] ,[] ]
+        for _ in range(len(nums) + 1): # loop through our nums populate our freq with [[], [], [], []]
+            freq.append([]) # add []
+        for n in nums: # loop through nums
+            count[n] = 1 + count.get(n, 0) # add one to existing value
+        for n,c in count.items(): #loop through our whole hashmap
+            freq[c].append(n) # at the frequency, we're going to append the numb
+        res = [] # answer array
+        for i in range(len(freq) - 1, 0, -1): # starting from the end, let's add the num to the ans
+            for n in freq[i]: # this goes through the actual list from the end and append's the value
+                res.append(n) #append the value to ans
+                if len(res) == k: # one we reach k, we can exit
+                    return res # return ans
