e45fc9858f342fef6243f41c9ff2ee5d1b59825c
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-01-16 21:08:33 -0500

    Time: 505 ms (77.92%) | Memory: 22.5 MB (8.21%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,34 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort() # 1. Sort in-place (don't assign to a variable)
-        ans = []
+        ans = [] 
         n = len(nums)
-        
-        for i in range(n - 2):
-            # 2. Skip duplicates for the fixed number
-            if i > 0 and nums[i] == nums[i-1]:
+        for i in range(n):
+            if nums[i] > 0:
+                break
+            elif i > 0 and nums[i] == nums[i - 1]:
                 continue
-            # 3. Initialize two pointers: left (j) and right (k)
-            j = i + 1
-            k = n - 1
-            while j < k:
-                curr_sum = nums[i] + nums[j] + nums[k]
-                if curr_sum == 0:
-                    ans.append([nums[i], nums[j], nums[k]])
-                    # 4. Skip duplicates for j and k to ensure distinct triplets
-                    while j < k and nums[j] == nums[j+1]:
-                        j += 1
-                    while j < k and nums[k] == nums[k-1]:
-                        k -= 1
-                    # Move both pointers inward after finding a match
-                    j += 1
-                    k -= 1
-                    
-                elif curr_sum < 0:
-                    # Sum is too small, move left pointer to the right to increase sum
-                    j += 1
+
+            lo, hi = i + 1, n - 1
+            while lo < hi:
+                curr = nums[i] + nums[lo] + nums[hi]
+                if curr == 0:
+                    ans.append([nums[i], nums[lo], nums[hi]])
+                    lo, hi = lo + 1, hi - 1
+                    while lo < hi and nums[lo] == nums[lo - 1]:
+                        lo += 1
+                    while lo < hi and nums[hi] == nums[hi + 1]:
+                        hi -= 1
+                elif curr < 0:
+                    lo += 1
                 else:
-                    # Sum is too big, move right pointer to the left to decrease sum
-                    k -= 1
-                    
+                    hi -= 1
         return ans
