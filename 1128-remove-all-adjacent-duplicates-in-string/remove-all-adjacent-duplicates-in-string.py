8ba6be59c7f37a01d47808ede4bc3b0526aa326c
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-01-17 16:08:51 -0500

    Time: 19 ms (80.44%) | Memory: 20.1 MB (17.97%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -5,10 +5,14 @@ class Solution:
         #3. Else, add it to the stack.
         #4. Return a string after all removals have been made. 
 
-        stack = []
-        for c in s:
-            if stack and c == stack[-1]:
+        stack = [] # Make stack
+        for c in s: # Loop through stack
+            if stack and c == stack[-1]: # If non-empty and top of stack is equal to c, pop 
                 stack.pop()
-            else:
+            else: # Else, append it.
                 stack.append(c)
         return "".join(stack)
+
+        # "abbaca" -> [a] -> [a, b] -> [a,b], c to be added is b & stack[-1] = b -> [a] -> [a] a c to be added is 
+        # a and stack[-1] is a -> [], stack empty so add c -> [c], [c, a]
+        
