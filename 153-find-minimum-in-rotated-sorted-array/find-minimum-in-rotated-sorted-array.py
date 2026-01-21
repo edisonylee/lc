bf71300e18f8d89671b6a29f544eb30909c3f416
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-01-20 19:48:04 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (7.03%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,4 +1,13 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # Given an array of length, n, sorted in ascending order is rotated between 1 and n times
-        return min(nums)
+        left = 0 # initialize pointers
+        right = len(nums) - 1
+        
+        while left < right: # while left is less than the right
+            mid = (left + right) // 2
+
+            if nums[mid] > nums[right]:
+                left = mid + 1 # [1,2,3,4,5]
+            else:
+                right = mid
+        return nums[left]
