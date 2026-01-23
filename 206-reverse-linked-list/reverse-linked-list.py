9054d1314ac8cd7a1dd45e6684e8aa4d86c34368
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-01-23 17:58:34 -0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (10.00%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -8,9 +8,8 @@ class Solution:
         prev = None
         curr = head
         while curr:
-            next_node = curr.next # first, make sure we don't lose the next node
-            curr.next = prev      # reverse the direction of the pointer
-            prev = curr           # set the current node to prev for the next node
-            curr = next_node      # move on
-            
+            nextNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextNode
         return prev
