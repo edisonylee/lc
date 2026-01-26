bdd17d4f01580cc770a94b734fb44f598cbe16bd
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-01-26 11:26:20 -0500

    Time: 19 ms (79.68%) | Memory: 20 MB (32.13%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -1,18 +1,9 @@
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        #1. Create a stack
-        #2. If the first element (on the stack) is equal to the current character, pop it.
-        #3. Else, add it to the stack.
-        #4. Return a string after all removals have been made. 
-
-        stack = [] # Make stack
-        for c in s: # Loop through stack
-            if stack and c == stack[-1]: # If non-empty and top of stack is equal to c, pop 
+        stack = []
+        for c in s:
+            if stack and stack[-1] == c:
                 stack.pop()
-            else: # Else, append it.
+            else:
                 stack.append(c)
         return "".join(stack)
-
-        # "abbaca" -> [a] -> [a, b] -> [a,b], c to be added is b & stack[-1] = b -> [a] -> [a] a c to be added is 
-        # a and stack[-1] is a -> [], stack empty so add c -> [c], [c, a]
-        
