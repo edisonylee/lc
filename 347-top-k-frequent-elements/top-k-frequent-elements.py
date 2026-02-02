ef890706909a5ebe3e4d6c2b9a8a79a80600eb3a
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-02-02 15:31:08 -0500

    Time: 15 ms (10.95%) | Memory: 25 MB (7.04%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,28 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        dic = {}
-        for num in nums:
+        # 1. Use a hashmap to store frequency.
+        # 2. Place these frequencies in a list of buckets.
+        # 3. Loop through from the end, and append to our ans. 
+
+        dic = {} # 1. frequency hashmap
+        for num in nums: 
             if num in dic:
                 dic[num] += 1
             else:
                 dic[num] = 1
-        freq_list = []
-        for _ in range(len(nums) + 1):
-            freq_list.append([])
-        for number, count in dic.items():
-            freq_list[count].append(number)
-        res = []
-        for i in range(len(freq_list) - 1, 0, -1):
-            for nums in freq_list[i]:
-                res.append(nums)
-                if len(res) == k:
-                    return res
+        
+        freq = [] # 2. build our empty buckets
+        for _ in range(len(nums) + 1): # [0, 1, 1, 3, 4, 5] -> [[], [0, 1, 3, 4, 5], [2], [], [], []]
+            freq.append([])
+        for num, count in dic.items(): # 3. Add nums to our array, i.e., (1,1,3,3) gives us...
+            freq[count].append(num) # -> [[], [], [3, 1], [], []]
+
+        ans = []
+        for i in range(len(freq) -1, 0, -1):
+            for num in freq[i]:
+                ans.append(num)
+                if len(ans) == k:
+                    return ans
+
+        
+
