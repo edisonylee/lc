be8671869e240e20fd69794349a7ac36646238c4
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-02-02 07:35:57 -0500

    Time: 0 ms (100.00%) | Memory: 20.7 MB (10.18%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # given an array nums, and int target, return indices of the two numbers such that they add up to target,
-        # you may assume that each input would have exactly ONE solution, and you may not use the same element twice.
         dic = {}
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in dic:
-                return [i, dic[complement]]
+                return [dic[complement], i]
             dic[nums[i]] = i
+
+                
