bf6555c3ef8aaa3840ad529dedb7419f2b0ba860
Edison Lee <113495642+edisonylee@users.noreply.github.com>
2026-02-02 07:47:34 -0500

    Time: 15 ms (42.79%) | Memory: 19.5 MB (36.70%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,24 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # So, given two strings, s and t, return true if t is an anagram of s,
-        # And false otherwise
-        # So, what is an anagram?
-        # An anagram is any string that contains all the letters, however mixed up, as another string
-        # With this in mind, we can start our implementation.
-        # Initial approach: Create dictionaries for each each string, then we can compare them by using .items
-        # This will allow us to fetch the appropriate key, value pairs. We can either loop through and compare 
-        # with a for loop, or just do the == for the hashMaps. 
-        # Let's try both out.
-        dicS = {}
-        dicT = {}
-        for chr in s:
-            if chr in dicS:
-                dicS[chr] += 1
-            else: 
-                dicS[chr] = 1
+        if len(s) != len(t):
+            return False
+        dic = {}
+        for chr in s: # store letters and their counts
+            if chr in dic:
+                dic[chr] += 1
+            else:
+                dic[chr] = 1
         for chr in t:
-            if chr in dicT:
-                dicT[chr] += 1
-            else: 
-                dicT[chr] = 1
-        return dicS == dicT
+            if chr in dic:
+                dic[chr] -= 1
+                if dic[chr] < 0:
+                    return False
+            else:
+                return False
+        return True
